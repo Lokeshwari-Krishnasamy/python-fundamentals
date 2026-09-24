fcee9994c4ee66ff13e47c6ac4216cb3c845379d
Lokeshwari Krishnasamy <lokeatjob@gmail.com>
2026-09-24 16:50:43 +0530

    Add Python variables examples

---
+++ 01-variables/variables.py
@@ -0,0 +1,14 @@
+# Python Foundations
+# Topic 01: Variables
+ 
+name = "Lokeshwari"
+role = "Software Engineer"
+experience = 10
+location = "Hyderabad"
+learning = "Python"
+ 
+print("Name:", name)
+print("Role:", role)
+print("Experience:", experience)
+print("Location:", location)
+print("Currently Learning:", learning)
